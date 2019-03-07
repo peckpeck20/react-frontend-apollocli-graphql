793179f9626b8991f2b0f2cd1d23ca455c168204
peckpeck20 <peckpeckthebeast@gmail.com>
2019-03-07 13:53:35 +0200

    signin added

--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,29 @@ import '../styles/App.css';
 import Post from './Post';
 import Signup from './Signup';
 import User from './User';
+import Signin from './Signin';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <Post />
-          <Signup />
           <User>
             {/* from payload children prop, desctructure data then destructure 2nd level User object */}
             {({ data: { user } }) => {
-              console.log(user)
-              return <p>user is logged in</p>
+              //console.log(user)
+              if (user) {
+                return <h1>signed in as {user.name} </h1>
+              }
+              else {
+                return null
+              }
             }}
           </User>
+          <Post />
+          <Signup />
+
+          <Signin />
         </div>
 
       </div>
---
+++ src/components/Signin.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { Mutation } from 'react-apollo';
+
+const SIGNIN_USER = gql`
+  mutation signin( $email: String!, $password: String!) {
+    signin(email :$email, password :$password) {
+      id
+      name,
+      email,
+      password,
+      posts {
+        title
+      }
+    }
+  }
+`;
+
+class Signin extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "xxxxxx@com",
+      password: "12312412"
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+  render() {
+    const { name, email, password } = this.state
+    return (
+      <Mutation
+        mutation={SIGNIN_USER}
+        variables={{ name, email, password }}
+        onCompleted={(data) => console.log(data)}
+        onError={(e) => { console.log(e) }}
+      >
+        {
+          (signin, { loading, error, data }) => {
+            if (error) return <p>{error.toString()}</p>
+            if (loading) return <h1>Submitting... one sec :D</h1>
+            if (data) return <p>sucess</p>
+
+            return (
+              <form
+                method="post"
+                onSubmit={e => {
+                  e.preventDefault();
+                  signin()
+                }}>
+                <fieldset
+                  disabled={loading}
+                  className="flex flex-column mt3"
+                >
+                  <h1>SIGN IN</h1>
+                  <input
+                    name="email"
+                    className="mb2"
+                    value={email}
+                    onChange={this.handleChange}
+                    type="text"
+                    placeholder="email"
+                  />
+                  <input
+                    name="password"
+                    className="mb2"
+                    value={password}
+                    onChange={this.handleChange}
+                    type="text"
+                    placeholder="password"
+                  />
+                  <button type="submit">signin</button>
+                </fieldset>
+              </form>
+            )
+          }
+        }
+      </Mutation>
+    );
+  }
+}
+
+export default Signin;
